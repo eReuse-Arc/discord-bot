47c2eebe7d4464dff2d40e209876c84691b22374
Ryan Yensch <ry2005@outlook.com>
2025-12-06 12:56:48 +1100

    added an invited leaderboard

--- cogs/leaderboards.py
+++ cogs/leaderboards.py
@@ -1,11 +1,37 @@
+import discord
 from discord.ext import commands
 
 
 class Leaderboards(commands.Cog):
-    @commands.command(help="Shows the leaderboard")
-    async def leaderboard(self, ctx):
-        await ctx.send("Coming soon!")
+    def __init__(self, bot):
+        self.bot = bot
 
+
+    @commands.command(help="Shows the invite leaderboard")
+    async def inviteleaderboard(self, ctx):
+        invites = await ctx.guild.invites()
+
+        invite_counts = {}
+
+        for invite in invites:
+            if invite.inviter:
+                invite_counts[invite.inviter] = invite_counts.get(invite.inviter, 0) + invite.uses
+
+        sorted_invites = sorted(invite_counts.items(), key=lambda x: x[1], reverse=True)
+
+        embed = discord.Embed(
+            title="Invite Leaderboard 🏆",
+            color=discord.Color.green()
+        )
+
+        for user, count in sorted_invites[:10]:
+            embed.add_field(
+                name= f"**{user.display_name}** - {count} invites",
+                value= "",
+                inline=False
+            )
+
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Leaderboards(bot))
