37e5a352637b64cd4c860967142668da6716332f
Ryan Yensch <ry2005@outlook.com>
2026-01-08 05:55:52 +1100

    added github command

--- cogs/general.py
+++ cogs/general.py
@@ -18,5 +18,13 @@ class General(commands.Cog):
     async def dm(self, interaction: discord.Interaction, message: str):
         await interaction.user.send(f"You said {message}")
         await interaction.response.send_message("Check your DM's :P", ephemeral=True)
+
+    @app_commands.command(name="github", description="Where the bots code is (Feel free to contribute 😁)")
+    async def github(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"### The github is [eReuse Bot]({'https://github.com/eReuse-Arc/discord-bot'}), you are extremely welcome to help develop any new features or fix other issues! 😁\n"
+            f"or to open any [issues]({'https://github.com/eReuse-Arc/discord-bot/issues'})"
+        )
+
 async def setup(bot):
     await bot.add_cog(General(bot))
