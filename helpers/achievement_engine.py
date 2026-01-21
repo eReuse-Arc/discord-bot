4365ba9cfb4d8c929819701aba05b85260e6c052
Ryan Yensch <ry2005@outlook.com>
2026-01-21 17:57:16 +1100

    changed the channel for achievements

--- helpers/achievement_engine.py
+++ helpers/achievement_engine.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 from helpers.achievements import ACHIEVEMENTS
 import discord
-from constants import GENERAL_CHANNEL_ID
+from constants import ACHIEVEMENT_UNLOCKS_CHANNEL_ID
 
 
 class AchievementEngine:
     def __init__(self, load_fn, save_fn):
         self.load = load_fn
         self.save = save_fn
-        self.channel_id = GENERAL_CHANNEL_ID
+        self.channel_id = ACHIEVEMENT_UNLOCKS_CHANNEL_ID
 
     async def _grant_role_if_needed(self, member: discord.Member, role_name: str | None):
         if not role_name:
